de4fc22cf59d8df18d75c6b9b4321c4f5f269323
kcyy <kcyy008@yahoo.com>
2020-10-03 08:58:30 +0800

    Update Selection.tsx
    change button

--- packages/page-extrinsics/src/Selection.tsx
+++ packages/page-extrinsics/src/Selection.tsx
@@ -55,7 +55,7 @@ function Selection (): React.ReactElement {
           icon='sign-in-alt'
           isDisabled={!extrinsic}
           isUnsigned
-          label={t<string>('Submit Unsigned')}
+          label={t<string>('Submit Unsigned_kcyy')}
           withSpinner
         />
         <TxButton
